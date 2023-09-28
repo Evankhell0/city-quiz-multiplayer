66626441352345392b3fdaa697e54ec4fd525d68
Evankhell <68199237+Evankhell0@users.noreply.github.com>
2023-09-28 12:36:14 +0200

    Added logic for stats about capitals

--- src/Stats.js
+++ src/Stats.js
@@ -2,14 +2,16 @@ class Stats {
     constructor(cities) {
         this.totalCities = cities.length;
         this.citiesGuessed = 0;
-        // no logic for values below yet
-        this.totalCapitals = 197;
+        this.totalCapitals = cities.filter(x => x.capital == true).length;
         this.capitalsGuessed = 0;
+
+        // no logic for values below yet
         this.totalGuesses = 0;
     }
 
     update(guessedCities) {
         this.citiesGuessed = guessedCities.length;
+        this.capitalsGuessed = guessedCities.filter(x => x.capital == true).length;
     }
 
     #toObj(name, current, total = null) {
