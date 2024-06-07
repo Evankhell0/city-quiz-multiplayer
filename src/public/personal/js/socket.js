f7fe2ecfd4edc417707e7a1c359796e77d67246d
Evankhell0 <68199237+Evankhell0@users.noreply.github.com>
2024-06-07 23:18:44 +0200

    WIP lobby list

--- src/public/personal/js/socket.js
+++ src/public/personal/js/socket.js
@@ -7,4 +7,27 @@ window.onload = function(e) {
 socket.on("playerData", (data) => {
     // Todo
         document.body.append(JSON.stringify(data));
+
+    const SAMPLE_LOBBIES = [
+        {id: 534, name: "Nikola's Lobby", type: 0},
+        {id: 727, name: "Evankhell's Lobby", type: 0},
+        {id: 999, name: "Cityquizzing", type: 1}
+    ];
+
+    SAMPLE_LOBBIES.forEach(x => createLobbyDiv(x));
 });
+
+const createLobbyDiv = (lobby) => {
+    const lobbyContainer = document.getElementById("lobbyContainer");
+    const item = document.createElement('div');
+
+    const span = document.createElement('span');
+    span.textContent = `[${lobby.id}] ${lobby.name} (${lobby.type})`;
+
+    const button = document.createElement('button');
+    button.innerHTML = "Join Lobby";
+
+    item.appendChild(span);
+    item.appendChild(button);
+    lobbyContainer.appendChild(item);
+}
